1ba392dc1c81c11aae50d34125954dbe7294952e
Jonathan Peppers <jonathan.peppers@gmail.com>
2014-06-10 08:42:05 -0500

    Added repro project

---
+++ MonoGameSounds.iOS/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MonoGameSounds.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        private TestGame _game;
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            _game = new TestGame();
+            _game.Run();
+			
+            return true;
+        }
+    }
+}
+
---
+++ MonoGameSounds.iOS/TestGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+
+namespace MonoGameSounds.iOS
+{
+    public class TestGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SoundEffectInstance _sound;
+        private TimeSpan _lastTime;
+
+        public TestGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+
+            Content.RootDirectory = "Content";
+
+            _graphics.IsFullScreen = true;
+            _graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+        }
+
+        protected override void LoadContent()
+        {
+            //NOTE: if you load chaingun_xnb (an XNB of the exact same file at lowest quality), it works fine
+
+            var sound = Content.Load<SoundEffect>("chaingun_mp3");
+            _sound = sound.CreateInstance();
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Red);
+
+            if (gameTime.TotalGameTime - _lastTime > TimeSpan.FromSeconds(2))
+            {
+                _lastTime = gameTime.TotalGameTime;
+
+                _sound.Play();
+            }
+        }
+    }
+}
+
